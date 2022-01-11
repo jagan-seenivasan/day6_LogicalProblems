fcdb82a9d60ead15ad5a8df93e6106798ce21756
jagan-seenivasan <85672216+jagan-seenivasan@users.noreply.github.com>
2022-01-11 22:19:43 +0530

    Execution Code

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace day_6_LogicalProblems
+{
+    class Program
+    {
+       public static void Main(string[] args)
+        {
+            //   FibanocciSeries Uc1 = new FibanocciSeries();
+            // FibanocciSeries.PrintFibanoci();
+            //  PerfectNumber uc2 = new PerfectNumber();
+            //  PerfectNumber.CheckPerfectNumber();
+            //  PrimeNumber uc3 = new PrimeNumber();
+            //  PrimeNumber.CheckTheNumberPrime();
+              ReverseAnumber uc4 = new ReverseAnumber();
+              ReverseAnumber.ReverseTheNumber();
+            //  CouponNumbers uc5 = new CouponNumbers();
+            //  CouponNumbers.PrintCouponCode();
+            //  vendingMachine uc6 = new vendingMachine();
+            // vendingMachine.CountTheCuurency();
+          //  DayOfWeek uc7 = new DayOfWeek();
+          //  DayOfWeek.CheckDayOfWeek();
+         //     TemperatureConversion Uc8 = new TemperatureConversion();
+          //  TemperatureConversion.convertCelToFera();
+
+        }
+    }
+}
